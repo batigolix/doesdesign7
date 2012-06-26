3e39b42feeb643f2a798c40e813557275736ea4b
Boris Doesborg <bdoesborg@gmail.com>
2012-06-26 07:08:28 +0200

    feature dd nieuws, shoami theme views templates,

---
+++ sites/all/themes/shoami/templates/html.tpl.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+
+<?php print $rdf_namespaces; ?>
+
+<head profile="<?php print $grddl_profile; ?>">
+
+	<meta charset="utf-8">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+
+  <?php print $head; ?>
+  <title><?php print $head_title; ?></title>
+  <?php print $styles; ?>
+  <?php print $scripts; ?>
+
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+
+<link href='http://fonts.googleapis.com/css?family=Fredoka+One' rel='stylesheet' type='text/css'>
+
+</head>
+<body class="<?php print $classes; ?>" <?php print $attributes;?>>
+  <div id="skip">
+    <a href="#main-menu"><?php print t('Jump to Navigation'); ?></a>
+  </div>
+
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+</body>
+</html>
---
+++ sites/all/themes/shoami/templates/page.tpl.php
@@ -0,0 +1,157 @@
+<div id="page" class="container <?php print $classes; ?>"<?php print $attributes; ?>>
+   <div id="header" class="sixteen columns clearfix">
+    <?php if ($logo): ?>
+      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+        <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
+      </a>
+    <?php endif; ?>
+
+    <?php if ($site_name || $site_slogan): ?>
+      <div id="name-and-slogan" class="eight columns alpha">
+
+        <?php if ($site_name): ?>
+          <?php if ($title): ?>
+            <div id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
+            </div>
+          <?php else: /* Use h1 when the content title is empty */ ?>
+            <h1 id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
+            </h1>
+          <?php endif; ?>
+        <?php endif; ?>
+
+        <?php if ($site_slogan): ?>
+          <div id="site-slogan"><?php print $site_slogan; ?></div>
+        <?php endif; ?>
+
+      </div>
+    <?php endif; ?>
+
+    <?php if ($page['header1']): ?>
+      <div id="header-region-1" class="three columns">
+        <?php print render($page['header1']); ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($page['header2']): ?>
+      <div id="header-region-2" class="three columns omega">
+        <?php print render($page['header2']); ?>
+      </div>
+    <?php endif; ?>
+
+  </div> <!-- /header -->
+
+  <div id="content_top"  class="sixteen columns clearfix">
+
+    <?php if ($page['content_top1']): ?>
+      <div id="content-top-1" class="ten columns alpha">
+        <?php print render($page['content_top1']); ?>
+      </div>
+    <?php endif; ?>
+
+    <?php if ($page['content_top2']): ?>
+      <div id="content_top2" class="six columns omega">
+        <?php print render($page['content_top2']); ?>
+      </div>
+    <?php endif; ?>
+
+  </div>
+
+
+
+  <!-- ______________________ MAIN _______________________ -->
+
+  <div id="main" class="clearfix sixteen columns">
+
+    <div id="content">
+      <div id="content-inner" class="inner column center">
+
+        <?php if ($breadcrumb || $title|| $messages || $tabs || $action_links): ?>
+          <div id="content-header">
+
+            <?php print $breadcrumb; ?>
+
+            <?php if ($page['highlight']): ?>
+              <div id="highlight"><?php print render($page['highlight']) ?></div>
+            <?php endif; ?>
+
+            <?php if ($title): ?>
+              <h1 class="title"><?php print $title; ?></h1>
+            <?php endif; ?>
+
+            <?php print render($title_suffix); ?>
+            <?php print $messages; ?>
+            <?php print render($page['help']); ?>
+
+            <?php if ($tabs): ?>
+              <div class="tabs"><?php print render($tabs); ?></div>
+            <?php endif; ?>
+
+            <?php if ($action_links): ?>
+              <ul class="action-links"><?php print render($action_links); ?></ul>
+            <?php endif; ?>
+            
+          </div> <!-- /#content-header -->
+        <?php endif; ?>
+
+        <div id="content-area">
+          <?php print render($page['content']) ?>
+        </div>
+
+        <?php print $feed_icons; ?>
+
+      </div>
+    </div> <!-- /content-inner /content -->
+
+    <?php if ($page['sidebar']): ?>
+      <div id="sidebar" class="column sidebar">
+          <?php print render($page['sidebar_first']); ?>
+      </div>
+    <?php endif; ?> <!-- /sidebar-first -->
+
+  </div> <!-- /main -->
+  
+  <!-- bunch of pseudeo regions for the homepage. prevent using panels n shite -->
+  
+    <div id="content_bot_row_1"  class="sixteen columns clearfix">
+    <?php if ($page['content_bot_row_1_1']): ?>
+      <div id="content-bot_row_1_1" class="ten columns alpha">
+        <?php print render($page['content_bot_row_1_1']); ?>
+      </div>
+    <?php endif; ?>
+    <?php if ($page['content_bot_row_1_2']): ?>
+      <div id="content_bot_row_1_2" class="six columns omega">
+        <?php print render($page['content_bot_row_1_2']); ?>
+      </div>
+    <?php endif; ?>
+  </div>
+
+    <div id="content_bot_row_2"  class="sixteen columns clearfix">
+    <?php if ($page['content_bot_row_2_1']): ?>
+      <div id="content-bot_row_2_1" class="five columns alpha">
+        <?php print render($page['content_bot_row_2_1']); ?>
+      </div>
+    <?php endif; ?>
+    <?php if ($page['content_bot_row_2_2']): ?>
+      <div id="content_bot_row_2_2" class="five columns">
+        <?php print render($page['content_bot_row_2_2']); ?>
+      </div>
+    <?php endif; ?>
+    <?php if ($page['content_bot_row_2_3']): ?>
+      <div id="content_bot_row_2_3" class="five columns omega">
+        <?php print render($page['content_bot_row_2_3']); ?>
+      </div>
+    <?php endif; ?>
+  </div>
+  
+
+  <!-- ______________________ FOOTER _______________________ -->
+
+  <?php if ($page['footer']): ?>
+    <div id="footer">
+      <?php print render($page['footer']); ?>
+    </div> <!-- /footer -->
+  <?php endif; ?>
+
+</div> <!-- /page -->
