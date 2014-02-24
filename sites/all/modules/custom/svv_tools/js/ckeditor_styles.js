8918b4ca6819f2eaafa4699e8accfb83b5954017
Boris Doesborg <bdoesborg@gmail.com>
2014-02-24 22:54:10 +0100

    add ckeditor styles set

---
+++ sites/all/modules/custom/svv_tools/js/ckeditor_styles.js
@@ -0,0 +1,10 @@
+CKEDITOR.stylesSet.add('svv',
+        [
+          {name: 'Rood', element: 'span', styles: {'color': 'red'}},
+          {name: 'Blauw', element: 'span', styles: {'color': 'blue'}},
+          {name: 'Geel', element: 'span', styles: {'color': 'yellow'}},
+          {name: 'Oranje', element: 'span', styles: {'color': 'orange'}},
+          {name: 'Groen', element: 'span', styles: {'color': 'greenß'}},
+          {name: 'Paars', element: 'span', styles: {'color': 'purple'}},
+          {name: 'Bruin', element: 'span', styles: {'color': '#630'}},
+        ]);
