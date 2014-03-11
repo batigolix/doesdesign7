a6c360d95b497d432856f751ad05dc64bbdf1492
boris doesborg <bdoesborg@gmail.com>
2014-03-11 22:14:28 +0100

    fix az browse veldeke. remove target blank from flickr js

--- sites/all/modules/custom/dd_tools/js/flickr.js
+++ sites/all/modules/custom/dd_tools/js/flickr.js
@@ -18,7 +18,7 @@
             // I only want the ickle square thumbnails
             var sourceSquare = (item.media.m).replace("_m.jpg", "_s.jpg");
             // Here's where we piece together the HTML
-            htmlString += '<li><a href="' + item.link + '" target="_blank">';
+            htmlString += '<li><a href="' + item.link + '" >';
             htmlString += '<img title="' + item.title + '" src="' + sourceSquare;
             htmlString += '" alt="';
             htmlString += item.title + '" />';
