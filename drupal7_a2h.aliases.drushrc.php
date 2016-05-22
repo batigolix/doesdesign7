f54bb3b1e2746d64f67eac1aaebdffd4e603a777
Boris Doesborg <bdoesborg@gmail.com>
2016-05-22 21:58:43 +0200

    aliases

---
+++ drupal7_a2h.aliases.drushrc.php
@@ -0,0 +1,37 @@
+<?php
+
+$aliases['svv.val'] = array(
+    'parent' => '@val',
+    'uri' => 'svv.val',
+    'root' => '/Users/boris/Sites/drupal7_a2h',
+    'path-aliases' => array(
+        '%files' => 'sites/svv.val/files',
+    ),
+);
+
+$aliases['svv.a2h'] = array(
+    'parent' => '@a2h',
+    'uri' => 'veldense-volkscultuur.nl',
+    'root' => '/home/doesborg/public_html/drupal7',
+    'path-aliases' => array(
+        '%files' => 'sites/veldense-volkscultuur.nl/files',
+    ),
+);
+
+$aliases['dd.val'] = array(
+    'parent' => '@val',
+    'uri' => 'doesdesign.val',
+    'root' => '/Users/boris/Sites/drupal7_a2h',
+    'path-aliases' => array(
+        '%files' => 'sites/doesdesign.val/files',
+    ),
+);
+
+$aliases['dd.a2h'] = array(
+    'parent' => '@a2h',
+    'uri' => 'doesdesign.nl',
+    'root' => '/home/doesborg/public_html/drupal7',
+    'path-aliases' => array(
+        '%files' => 'sites/doesdesign.nl/files',
+    ),
+);
